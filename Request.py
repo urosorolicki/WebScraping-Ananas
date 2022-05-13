d21c7e8677fce1d4d8c1c1fae9df4998958d4ae1
Uroš Orolicki <96778657+urosorolicki@users.noreply.github.com>
2022-05-13 13:08:30 +0200

    Testing / / /

--- Request.py
+++ Request.py
@@ -1,7 +1,11 @@
 import requests
+import bs4 as BeautifulSoup
 
 
-res = requests.get('https://ananas.rs/login')
+page = requests.get(
+    "https://ananas.rs/login")
+soup = BeautifulSoup(page.content, 'html.parser')
+# res = requests.get('https://ananas.rs/login')
 
 print(res.text)
 print(res.status_code)
