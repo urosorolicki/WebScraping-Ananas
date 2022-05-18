fe393f17740d5787c174eb460e994bb72c63af80
Uroš Orolicki <96778657+urosorolicki@users.noreply.github.com>
2022-05-18 16:37:39 +0200

    Edit

--- Request.py
+++ Request.py
@@ -5,19 +5,5 @@ import os
 
 res = requests.get('https://ananas.rs/login')
 
-for x in range(2000, 3000):
-    print('ID', x, sep=':')
-
-
-def write_file(res, op2.csv):
-    with open(op2.csv, 'wb') as outfile:
-
-
-f = open('op2.csv', 'w')
-writer = csv.writer(f)
-writer = csv.writer('op2.csv')
-for row in res:  # ABOVE ERROR IS THROWN HERE
-    writer.writerow(row)
-f.close()
-print(res.text)
-print(res.status_code)
+#Needed to be done till tomorrow ">.<"
+#Add writer, Json or csv
