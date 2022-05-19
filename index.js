ae3a82aca5599c4d65d0acc7bfdd295475ba2864
Uroš Orolicki <96778657+urosorolicki@users.noreply.github.com>
2022-05-19 09:08:20 +0200

    Edit

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const port = 5000;                  
 
 
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      
+app.get('webscraping/index.js', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('index.js', {root: __dirname});      
                                                         
 });
 
 app.listen(port, () => {         
-    console.log(`Now listening on port ${port}`); 
+    console.log(`5000 ${port}`); 
 });
